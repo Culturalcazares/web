521ae15cff985da47bfa02089ff9dc738054ea6c
Raúl Bejarano <raulbejaranoparrilla@gmail.com>
2025-11-05 11:58:53 +0100

    ✨ Vistas con placeholders

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,5 +3,15 @@
 <template>
   <div class="container py-4 my-4">
     <h1 class="mb-4">Somos Culturalcázares</h1>
+    <img class="img-fluid" src="/images/logo.png" alt="Logo" />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim orci ut libero lacinia, iaculis consequat elit luctus. Ut porttitor aliquet sem, eget tempus nunc pretium fermentum. Ut eget libero purus. Nullam sit amet massa dolor. Aenean ultrices urna ultricies, iaculis nibh eget, feugiat velit. Sed a magna id tortor ornare elementum. Sed ornare, elit et tincidunt faucibus, sapien neque ultrices erat, in viverra nisl dolor condimentum neque. Morbi mattis eros sit amet purus pellentesque, non aliquet nulla tincidunt.
+    </p>
+    <p>
+      Cras sit amet ligula at justo congue vulputate eu in diam. Ut ut lectus libero. Nam consectetur, lectus in cursus laoreet, felis ipsum gravida tellus, scelerisque hendrerit dui nisl in tortor. Fusce porta eleifend ante, in vehicula dui. Curabitur luctus dolor commodo, fringilla orci a, hendrerit leo. Curabitur lacinia augue ante, eget venenatis justo tincidunt at. Praesent eu arcu commodo, porta est nec, blandit est. Aenean faucibus porttitor leo, eu egestas orci lacinia ultricies. Donec quis bibendum ligula.
+    </p>
+    <p>
+      In tincidunt pharetra elit id ornare. Cras bibendum hendrerit laoreet. Aliquam turpis lectus, mattis at maximus sit amet, dapibus nec enim. Ut pulvinar mauris lacinia leo dignissim convallis. Nullam sed tellus molestie, commodo eros quis, porta sem. Vestibulum rhoncus sit amet massa eu suscipit. Morbi ut orci id purus tincidunt mattis. Aenean sed tortor a arcu blandit dignissim.
+    </p>
   </div>
 </template>
